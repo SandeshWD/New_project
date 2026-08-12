28438693ac02bb7910dea9eefe51f6b277345ff8
SandeshWD <wdsandesh@gmail.com>
2026-08-12 11:49:43 +0530

    Add project files.

---
+++ TestYourSpeed/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TestYourSpeed.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

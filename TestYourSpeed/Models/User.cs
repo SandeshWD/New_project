c4000b84eb42c720033ff17d109c77f420aafeed
SandeshWD <wdsandesh@gmail.com>
2026-08-15 22:14:34 +0530

    Add User model

--- TestYourSpeed/Models/User.cs
+++ TestYourSpeed/Models/User.cs
@@ -1,6 +1,9 @@
-﻿namespace TestYourSpeed.Models
+﻿
+namespace TestYourSpeed.Models
 {
     public class User
     {
+        public int Username { get; set; }
+        public string Password { get; set; }
     }
 }
